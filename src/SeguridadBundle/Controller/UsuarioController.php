b56e3fcca809b2fc06168f600567cf4265ee7b9b
Javier Almanzor <desdeotralogica@gmail.com>
2018-11-10 20:38:26 +0100

    version 1.6

---
+++ src/SeguridadBundle/Controller/UsuarioController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SeguridadBundle\Controller;
+
+use SeguridadBundle\Entity\Usuario;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsuarioController extends Controller {
+    
+    /**
+    * @Route("/hola-mundo", name="hola_mundo")
+    */
+    public function nadaAction(Request $request) {
+
+        return $this->render('@Seguridad/Default/index.html.twig', []);
+    }
+}
+
+?>
